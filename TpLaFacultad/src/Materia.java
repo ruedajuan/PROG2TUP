96be4c360417ef077f264b4b2272a52fdfd14821
Juan I <111068326+ruedajuan@users.noreply.github.com>
2022-09-29 10:22:34 -0300

    Update Materia.java

--- TpLaFacultad/src/Materia.java
+++ TpLaFacultad/src/Materia.java
@@ -4,13 +4,13 @@ import java.util.List;
 public class Materia implements Informacion{
     private String nombre;
     private Profesor titular;
-    private List<Estudiante> coleccionEstudiantes= new ArrayList<Estudiante>();
+    private List<Estudiante> coleccionEstudiantes=new ArrayList<Estudiante>();
 
     public Materia(){
 
     }
 
-    public Materia(String nombre,Profesor titular, List coleccionEstudiantes){
+    public Materia(String nombre,Profesor titular, List<Estudiante> coleccionEstudiantes){
         this.nombre=nombre;
         this.titular=titular;
         this.coleccionEstudiantes=coleccionEstudiantes;
@@ -28,32 +28,48 @@ public class Materia implements Informacion{
     public void setTitular(Profesor titular){
         this.titular=titular;
     }
-    public void setColeccionEstudiantes(List coleccionEstudiantes){
+    public void setColeccionEstudiantes(List<Estudiante> coleccionEstudiantes){
         this.coleccionEstudiantes=coleccionEstudiantes;
     }
-    public List getColeccionEstudiantes(){
+    public List<Estudiante> getColeccionEstudiantes(){
         return coleccionEstudiantes;
     }
     public void agregarEstudiante (Estudiante estudiante){
         coleccionEstudiantes.add(estudiante);
     }
-    public void eliminarEstudiante(Estudiante estudiante){
-        coleccionEstudiantes.remove(estudiante);
+    public void eliminarEstudiante(String estudianteApellidoABorrar){
+        for(int i=0;i<coleccionEstudiantes.size();i++){
+            if(coleccionEstudiantes.get(i).getApellido().equals(estudianteApellidoABorrar)){
+                coleccionEstudiantes.remove(i);
+            }
+        }
+
     }
     public void modificarTitular(Profesor titular){
         this.titular=titular;
     }
- @Override
+
+
+    @Override
     public int verCantidad() {
         int x =coleccionEstudiantes.size();
-        System.out.println(x);
+        System.out.println("Estudiantes en "+getNombre()+" :"+x);
         return x;
     }
 
     @Override
     public String listarContenidos() {
+        int salida;
+        
         return null;
     }
 
-
+    @Override
+    public String toString() {
+        return "Materia{" +
+                "nombre='" + nombre + '\'' +
+                ", titular=" + titular +
+                ", coleccionEstudiantes=" + coleccionEstudiantes +
+                '}';
+    }
 }
