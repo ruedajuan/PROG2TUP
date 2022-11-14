51cff360577bbec1a40352628dc5683e26a540c4
Juan I <111068326+ruedajuan@users.noreply.github.com>
2022-11-14 15:37:04 -0300

    Update Equipos.java

--- TrabajoFinalLab22022/src/Logica/Equipos.java
+++ TrabajoFinalLab22022/src/Logica/Equipos.java
@@ -7,13 +7,37 @@ import java.util.List;
 public class Equipos implements Informacion, Serializable {
     private String nombre;
     private String ciudad;
+    private int codigo;
     private List<Jugadores> plantilla = new ArrayList<Jugadores>();
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public List<Jugadores> getPlantilla(){
+        return plantilla;
+    }
+    public void setPlantilla(List<Jugadores>plantilla){
+        this.plantilla=plantilla;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+
+
     public Equipos (){
-    };
-    public Equipos(String nombre, String ciudad){
+    }
+    public Equipos(String nombre, String ciudad, int codigo){
         this.nombre=nombre;
         this.ciudad=ciudad;
+        this.codigo=codigo;
     }
     public void agregarJugadores(Jugadores jugador){
         plantilla.add(jugador);
@@ -34,4 +58,7 @@ public class Equipos implements Informacion, Serializable {
         }
         return null;
     }
+
+
 }
+
